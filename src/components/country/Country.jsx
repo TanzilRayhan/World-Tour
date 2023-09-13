869b34d8f19b68868934ca22e74bcf5c913fcdea
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-09-13 09:39:32 +0600

    country

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,10 +1,12 @@
+import './country.css'
 
 const Country = ({country}) => {
     // console.log(country);
-    const{name,   } = country;
+    const{name, flags,  } = country;
     return (
-        <div>
+        <div className='country'>
             <h3>Name: {name?.common}</h3>
+            <img src={flags.png} alt="" />
         </div>
     );
 };
